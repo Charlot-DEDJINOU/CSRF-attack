a7fd467c1ee9cd1df8ed6366de42c59346f04bca
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-06-17 11:31:16 +0100

    one champ

--- backend/src/models/cookieModel.js
+++ backend/src/models/cookieModel.js
@@ -1,8 +1,7 @@
 const mongoose = require('../config/moogose');
 
 const cookieSchema = new mongoose.Schema({
-  id : { type: String, required: true },
-  session : { type: String, required: true },
+  chocolate : { type: String, required: true },
 });
 
 const Cookie = mongoose.model('Cookies', cookieSchema);
