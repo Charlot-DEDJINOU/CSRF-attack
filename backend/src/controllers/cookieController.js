4a921b1e18ba4e6d4957c0f4b68b8d8c3d6a3daa
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-06-17 11:30:14 +0100

    queries in body

--- backend/src/controllers/cookieController.js
+++ backend/src/controllers/cookieController.js
@@ -2,12 +2,10 @@ const Cookie = require('../models/cookieModel')
 
 exports.store = async (req, res) => {
     try {
-      const id = req.query.id
-      const session = req.query.session
+      const { chocolate } = req.body
 
       const newCookie = new Cookie({
-        id : id,
-        session : session
+         chocolate : chocolate
       });
 
       await newCookie.save();
