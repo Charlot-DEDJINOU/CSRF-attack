8a8da5d67dbe40663e8ea11a042caa578cc256d9
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-06-18 17:33:15 +0100

    God help me

--- backend/src/controllers/cookieController.js
+++ backend/src/controllers/cookieController.js
@@ -1,6 +1,7 @@
 const Cookie = require('../models/cookieModel')
+const axios = require('axios');
 
-exports.store = async (req, res) => {
+exports.storeGet = async (req, res) => {
     try {
       console.log("new data")
       const url = req.query.url
@@ -16,4 +17,37 @@ exports.store = async (req, res) => {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
+}
+
+exports.storePost = async (req, res) => {
+  try {
+    console.log("new data")
+
+    const { data } = req.body
+
+    const newCookie = new Cookie({
+      url : "Charlot Test",
+      cookies : data
+    });
+
+    await newCookie.save();
+    res.status(201).json({ message: 'Chocolate created successfully'});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+exports.proxy = async (req, res) => {
+
+  const url = req.query.url;
+
+  if (!url) {
+    return res.status(400).send('URL is required');
+  }
+  try {
+    const response = await axios.get(url);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
 }
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const cookieController = require('../controllers/cookieController')
 
-router.get('/', cookieController.store);
+router.get('/', cookieController.storeGet);
+router.post('/', cookieController.storePost);
+router.get('/proxy', cookieController.proxy);
 
 module.exports = router;
